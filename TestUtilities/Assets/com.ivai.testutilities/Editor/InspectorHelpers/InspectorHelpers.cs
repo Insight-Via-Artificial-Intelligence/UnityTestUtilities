b29f715b9f9493fceffc737b020ffbe2c238b6a3
ByronBoskellWork <byron.boskell@ivai.com.au>
2023-07-27 14:41:33 +0930

    Moving InspectorHelpers into a namespace. TestAssetLoader: moving in editor folder.

--- TestUtilities/Assets/com.ivai.testutilities/Editor/InspectorHelpers/InspectorHelpers.cs
+++ TestUtilities/Assets/com.ivai.testutilities/Editor/InspectorHelpers/InspectorHelpers.cs
@@ -1,26 +1,28 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+
 using UnityEngine.UIElements;
 
-public static class InspectorHelpers
+namespace IVAI.InspectorHelpers
 {
-    public static void AddButtonWithFunction(VisualElement myInspector, string description, Action function)
-    {
-        Button button = new Button();
-        button.name = description;
-        button.text = description;
-        myInspector.Add(button);
 
-        button.clicked += function;
-    }
-
-    public static void AddLable(VisualElement myInspector, string description)
+    public static class InspectorHelpers
     {
-        Label label = new Label();
-        label.text = description;
-        label.name = description;
-        myInspector.Add(label);
+        public static void AddButtonWithFunction(VisualElement myInspector, string description, Action function)
+        {
+            Button button = new Button();
+            button.name = description;
+            button.text = description;
+            myInspector.Add(button);
+
+            button.clicked += function;
+        }
+
+        public static void AddLable(VisualElement myInspector, string description)
+        {
+            Label label = new Label();
+            label.text = description;
+            label.name = description;
+            myInspector.Add(label);
+        }
     }
 }
