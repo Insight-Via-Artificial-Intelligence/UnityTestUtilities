d130ff94d8c47a0a4a28545c6988b15152e86b0f
ByronBoskellWork <byron.boskell@ivai.com.au>
2023-07-27 14:45:37 +0930

    InspectorHelpers/TestAssetLoader: Removing need namespace.

--- TestUtilities/Assets/com.ivai.testutilities/Editor/InspectorHelpers/InspectorHelpers.cs
+++ TestUtilities/Assets/com.ivai.testutilities/Editor/InspectorHelpers/InspectorHelpers.cs
@@ -2,27 +2,23 @@ using System;
 
 using UnityEngine.UIElements;
 
-namespace IVAI.InspectorHelpers
+public static class InspectorHelpers
 {
-
-    public static class InspectorHelpers
+    public static void AddButtonWithFunction(VisualElement myInspector, string description, Action function)
     {
-        public static void AddButtonWithFunction(VisualElement myInspector, string description, Action function)
-        {
-            Button button = new Button();
-            button.name = description;
-            button.text = description;
-            myInspector.Add(button);
+        Button button = new Button();
+        button.name = description;
+        button.text = description;
+        myInspector.Add(button);
 
-            button.clicked += function;
-        }
+        button.clicked += function;
+    }
 
-        public static void AddLable(VisualElement myInspector, string description)
-        {
-            Label label = new Label();
-            label.text = description;
-            label.name = description;
-            myInspector.Add(label);
-        }
+    public static void AddLable(VisualElement myInspector, string description)
+    {
+        Label label = new Label();
+        label.text = description;
+        label.name = description;
+        myInspector.Add(label);
     }
 }
